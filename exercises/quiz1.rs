9baa17be5643957fe843c8f5143d575fa4d7b517
liubingrun <liubr1@chinatelecom.cn>
2023-07-05 14:54:12 +0800

    finish quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(count: i32) -> i32 {
+    if count > 40 
+        {count}
+    else if count > 0
+        {count * 2}
+    else {
+        0
+    }
+}
 
 // Don't modify this function!
 #[test]
