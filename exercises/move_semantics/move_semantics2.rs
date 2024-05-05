434f5d303feeef756b21715c5ef192eb74a3aa63
liubingrun <liubr1@chinatelecom.cn>
2024-05-05 14:43:15 +0800

    第一次半途而废的练习
    Signed-off-by: liubingrun <liubr1@chinatelecom.cn>

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -10,23 +10,20 @@
 // I AM NOT DONE
 
 fn main() {
-    let vec0 = Vec::new();
+    let mut vec0 = Vec::new();
 
+    // Do not move the following line!
     let mut vec1 = fill_vec(vec0);
 
     println!("{} has length {}, with contents: `{:?}`", "vec0", vec0.len(), vec0);
 
-    vec1.push(88);
+    vec0.push(88);
 
     println!("{} has length {}, with contents `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
-
+fn fill_vec(vec: &mut Vec<i32>) {
     vec.push(22);
     vec.push(44);
     vec.push(66);
-
-    vec
 }
--- exercises/strings/strings1.rs
+++ exercises/strings/strings1.rs
@@ -5,13 +5,36 @@
 // Execute `rustlings hint strings1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
     let answer = current_favorite_color();
     println!("My current favorite color is {}", answer);
+
+    let mut heap = vec![123];
+    heap.push(5);
+    heap.push(9);
+    heap.push(2);
+    heap.push(1);
+
+    // [1,5)
+    for i in 1..5 {
+        println!("{i}");
+    }
+
+    for x in &heap {
+        print!("{x} ")
+    }
+
+    let fifth = heap.get(6);
+    match fifth {
+        Some(fifth) => println!(" fifth: {fifth}"),
+        None => println!("There is no fifth element"),
+    }
+
+
 }
 
 fn current_favorite_color() -> String {
-    "blue"
+    String::from("blud")
+    // "blue".to_string()
 }
