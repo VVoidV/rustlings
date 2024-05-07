6ec6348b5cecd61454c325c1bec34236107b805b
LiuBingrun <liubr1@chinatelecom.cn>
2024-05-07 08:47:50 +0800

    finish quize3
    with help of copilot
    
    Signed-off-by: LiuBingrun <liubr1@chinatelecom.cn>

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,15 +16,27 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+pub enum Grade {
+    Numeric(f32),
+    Alphabetic(String),
+}
 
-pub struct ReportCard {
-    pub grade: f32,
+impl std::fmt::Display for Grade {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Grade::Numeric(n) => write!(f, "{}", n),
+            Grade::Alphabetic(s) => write!(f, "{}", s),
+        }
+    }
+}
+
+pub struct ReportCard<T> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl<T: std::fmt::Display> ReportCard<T> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
@@ -38,7 +50,7 @@ mod tests {
     #[test]
     fn generate_numeric_report_card() {
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Grade::Numeric(2.1),
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
         };
@@ -52,7 +64,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Grade::Alphabetic("A+".to_string()),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
